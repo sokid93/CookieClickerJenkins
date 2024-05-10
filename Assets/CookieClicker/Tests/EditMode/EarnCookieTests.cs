f4942860367be26008cae15b6101294c7ffc677f
unknown <daniel.hurtadoramirez314@gmail.com>
2024-05-10 18:45:16 +0200

    test: EarnCookies

--- Assets/CookieClicker/Tests/EditMode/EarnCookieTests.cs
+++ Assets/CookieClicker/Tests/EditMode/EarnCookieTests.cs
@@ -2,5 +2,25 @@
 
 public class EarnCookieTests
 {
-    
+    [Test]
+    public void EarnCookies()
+    {
+        var cookieClicker = new CookieClicker();
+        var sut = new EarnCookie(cookieClicker);
+        sut.Execute();
+        Assert.AreEqual(1, cookieClicker.Cookies);
+    }
+}
+public class EarnCookie
+{
+    CookieClicker cookieClicker;
+    public EarnCookie(CookieClicker cookieClicker)
+    {
+        this.cookieClicker = cookieClicker;
+    }
+
+    public void Execute()
+    {
+        cookieClicker.EarnCookie();
+    }
 }
