32169dcab53d50a2863aeac07f3ed485132578ba
JavierStark <javiertorralbocortes@gmail.com>
2024-05-10 18:58:39 +0200

    test: signal to cookie counter

--- Assets/CookieClicker/Tests/EditMode/EarnCookieTests.cs
+++ Assets/CookieClicker/Tests/EditMode/EarnCookieTests.cs
@@ -10,17 +10,48 @@ public class EarnCookieTests
         sut.Execute();
         Assert.AreEqual(1, cookieClicker.Cookies);
     }
+
+    [Test]
+    public void SignalToCookieCounter()
+    {
+        var cookieClicker = new CookieClicker();
+        var outputCounter = new MockOutputCounter();
+        var sut = new EarnCookie(cookieClicker, outputCounter);
+        sut.Execute();
+        Assert.IsTrue(outputCounter.updated);
+    }
 }
+
+public class MockOutputCounter : IOutputCounter
+{
+    public bool updated = false;
+    public void UpdateCounter() => updated = true;
+}
+
+public interface IOutputCounter
+{
+    void UpdateCounter();
+}
+
 public class EarnCookie
 {
     CookieClicker cookieClicker;
+    private readonly IOutputCounter outputCounter;
+
     public EarnCookie(CookieClicker cookieClicker)
     {
         this.cookieClicker = cookieClicker;
     }
 
+    public EarnCookie(CookieClicker cookieClicker, IOutputCounter outputCounter)
+    {
+        this.cookieClicker = cookieClicker;
+        this.outputCounter = outputCounter;
+    }
+
     public void Execute()
     {
         cookieClicker.EarnCookie();
+        outputCounter.UpdateCounter();
     }
 }
