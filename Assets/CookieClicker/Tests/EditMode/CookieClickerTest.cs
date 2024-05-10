b0d455dd278805b93912803ccdd5018d5eb09cc8
unknown <daniel.hurtadoramirez314@gmail.com>
2024-05-10 18:10:45 +0200

    build: make tests

---
+++ Assets/CookieClicker/Tests/EditMode/CookieClickerTest.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class CookieClickerTest
+{
+    // A Test behaves as an ordinary method
+    [Test]
+    public void CookieClickerTestSimplePasses()
+    {
+        // Use the Assert class to test conditions
+    }
+}
