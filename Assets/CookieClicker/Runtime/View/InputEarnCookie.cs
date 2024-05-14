3bf67a451a7ec41c17288299c2f477b81096d14a
adrianriverof <adrianriverof@gmail.com>
2024-05-14 18:56:37 +0200

    build: cookieclicker view

--- Assets/CookieClicker/Runtime/View/InputEarnCookie.cs
+++ Assets/CookieClicker/Runtime/View/InputEarnCookie.cs
@@ -3,8 +3,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class InputEarnCookie : MonoBehaviour
 {
+    private EarnCookie presenter;
     private void Start()
     {
         GetComponent<UnityEngine.UI.Button>().onClick.AddListener(PressButton);
