be42635bf9afaa3eeaca7b13f707658ed94d4459
Javierdds <deduenassantanojavier@gmail.com>
2024-05-30 17:59:45 +0200

    test: dasghdbas

---
+++ Assets/CookieClicker/Tests/EditMode/EarnCookiesByTimeTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace CookieClicker.Tests.EditMode
+{
+    public class EarnCookiesByTimeTests
+    {
+        [Test]
+        public void fanhusdsdnf()
+        {
+            var cookieClicker = CookieClicker.CreateEmpty();
+            var outputCounter = new MockOutputCounter();
+            var earnCookiePresenter = new EarnCookie(cookieClicker, outputCounter);
+            var sut = new EarnCookiesByTime(earnCookiePresenter);
+
+            sut.Execute(new DateTime());
+
+            Assert.AreEqual(0, cookieClicker.Cookies);
+        }
+    }
+
+    public class EarnCookiesByTime
+    {
+        public EarnCookiesByTime(EarnCookie earnCookiePresenter)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Execute(DateTime dateTime)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
