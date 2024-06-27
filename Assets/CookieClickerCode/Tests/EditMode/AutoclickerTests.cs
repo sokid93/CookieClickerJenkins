deb57bec5bbec063865960df730572e96a01fa74
knexator <daniel.hurtadoramirez314@gmail.com>
2024-06-27 16:40:33 +0200

    test: replicate weird error

---
+++ Assets/CookieClickerCode/Tests/EditMode/AutoclickerTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using CookieClickerCode.Runtime.Domain;
+using CookieClickerCode.Runtime.Presenter;
+using NUnit.Framework;
+
+namespace CookieClickerCode.Tests.EditMode
+{
+    public class AutoclickerTests
+    {
+        [Test]
+        public void asdfasdf() {
+            var cookieClicker = CookieClicker.CreateEmpty();
+            var sut = new Autoclicker(cookieClicker);        
+            cookieClicker.ClicksPerSecond = 2;
+            
+            var base_date = new DateTime();
+            sut.AccumulateTime(base_date);
+            Assert.IsFalse(sut.MustAutoclick(base_date + TimeSpan.FromSeconds(0.3f)));
+        }
+    }
+}
