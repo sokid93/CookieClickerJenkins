234158a009b28a0f35f8afec203affff43890904
adrianriverof <adrianriverof@gmail.com>
2024-06-27 16:56:29 +0200

    test: click automatically when 0.5 seconds have passed

--- Assets/CookieClickerCode/Tests/EditMode/AutoclickerTests.cs
+++ Assets/CookieClickerCode/Tests/EditMode/AutoclickerTests.cs
@@ -17,5 +17,19 @@ namespace CookieClickerCode.Tests.EditMode
             sut.AccumulateTime(base_date);
             Assert.IsFalse(sut.MustAutoclick(base_date + TimeSpan.FromSeconds(0.3f)));
         }
+
+        [Test]
+        public void MustAutoclick()
+        {
+            var cookieClicker = CookieClicker.CreateEmpty();
+            var sut = new Autoclicker(cookieClicker);        
+            cookieClicker.ClicksPerSecond = 2;
+            
+            var base_date = new DateTime();
+            sut.AccumulateTime(base_date);
+            Assert.IsTrue(sut.MustAutoclick(base_date + TimeSpan.FromSeconds(0.8)));
+            Assert.IsTrue(sut.MustAutoclick(base_date + TimeSpan.FromSeconds(0.5)));
+
+        }
     }
 }
