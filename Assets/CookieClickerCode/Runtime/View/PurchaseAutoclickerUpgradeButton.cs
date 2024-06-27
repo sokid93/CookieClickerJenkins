a088f44f1da6dbb2273ba2f28a2223f241be602d
JavierStark <javiertorralbocortes@gmail.com>
2024-06-27 16:20:21 +0200

    feat: autoclicker purchase view

---
+++ Assets/CookieClickerCode/Runtime/View/PurchaseAutoclickerUpgradeButton.cs
@@ -0,0 +1,33 @@
+﻿using CookieClickerCode.Runtime.Presenter;
+using UnityEngine;
+
+namespace CookieClickerCode.Runtime.View
+{
+    public class PurchaseAutoclickerUpgradeButton : MonoBehaviour
+
+    {
+        private PurchaseAutoclickerUpgrade presenter;
+        private QueryCookies queryCookies;
+
+        public void Configure(PurchaseAutoclickerUpgrade presenter, QueryCookies queryCookies)
+        {
+            this.presenter = presenter;
+            this.queryCookies = queryCookies;
+        }
+
+        private void Start()
+        {
+            GetComponent<UnityEngine.UI.Button>().onClick.AddListener(PressButton);
+        }
+
+        private void PressButton()
+        {
+            presenter.Execute();
+        }
+
+        private void Update()
+        {
+            GetComponent<UnityEngine.UI.Button>().enabled = queryCookies.Execute();
+        }
+    }
+}
