f886ece080ca0288e868ab83b1e6c61dcb277eb4
Francisco Jose Soriano Ruiz <franbarrabajakh@gmail.com>
2025-08-15 10:35:29 +0200

    ci: build script and jenkinsfile

---
+++ Assets/Jenkins/BuildScript.cs
@@ -0,0 +1,57 @@
+using UnityEditor;
+using UnityEngine;
+using System.IO;
+using System.Linq;
+
+public static class BuildScript
+{
+    public static void Build()
+    {
+        PlayerSettings.bundleVersion = "1.0.0";
+        PlayerSettings.productName = "CookieClicker";
+        
+        string buildPath = Path.Combine(Directory.GetCurrentDirectory(), "Build", $"CookieClicker.exe");
+        Directory.CreateDirectory(Path.GetDirectoryName(buildPath));
+        
+        string[] scenes = GetScenes();
+        if (scenes.Length == 0)
+        {
+            Debug.LogError("No scenes found!");
+            EditorApplication.Exit(1);
+            return;
+        }
+        
+        BuildOptions options = BuildOptions.None;
+        
+        BuildPlayerOptions buildOptions = new BuildPlayerOptions
+        {
+            scenes = scenes,
+            locationPathName = buildPath,
+            target = BuildTarget.StandaloneWindows64,
+            options = options
+        };
+        
+        var report = BuildPipeline.BuildPlayer(buildOptions);
+        
+        if (report.summary.result == UnityEditor.Build.Reporting.BuildResult.Succeeded)
+        {
+            Debug.Log("Build succeeded!");
+            EditorApplication.Exit(0);
+        }
+        else
+        {
+            Debug.LogError("Build failed!");
+            EditorApplication.Exit(1);
+        }
+    }
+    
+    private static string[] GetScenes()
+    {
+        var scenes = EditorBuildSettings.scenes;
+        if (scenes.Length > 0)
+            return scenes.Where(s => s.enabled).Select(s => s.path).ToArray();
+        
+        string[] guids = AssetDatabase.FindAssets("t:Scene");
+        return guids.Select(guid => AssetDatabase.GUIDToAssetPath(guid)).ToArray();
+    }
+}
---
+++ Jenkinsfile
@@ -0,0 +1,35 @@
+pipeline {
+    agent any
+    
+    environment {
+        UNITY_PATH = "C:\Program Files\Unity\Hub\Editor\2022.3.9f1\Editor\\Unity.exe" // CAMBIAD ESTO
+        REPO_URL = "https://github.com/sokid93/CookieClickerJenkins.git"
+    }
+    
+    stages {
+        stage('Checkout') {
+            steps {
+                cleanWs()
+                bat "git clone ${REPO_URL} ."
+            }
+        }
+        
+        stage('Test') {
+            steps {
+                bat """
+                    if not exist "CI" mkdir "CI"
+                    "${UNITY_PATH}" -runTests -projectPath "%WORKSPACE%" -exit -batchmode -testResults "%WORKSPACE%\\CI\\results.xml" -testPlatform EditMode
+                """
+            }
+        }
+        
+        stage('Build') {
+            steps {
+                bat """
+                    "${UNITY_PATH}" -executeMethod SimpleBuildScript.Build -projectPath "%WORKSPACE%" -quit -batchmode
+                """
+                archiveArtifacts artifacts: 'Build/**/*', fingerprint: true
+            }
+        }
+    }
+}
